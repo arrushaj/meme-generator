a69e6c5e8b45ab5d7a15080a7047867b7d7fa1b6
Ardit Rrushaj <arditrrushaj@gmail.com>
2022-10-29 19:18:37 -0500

    Finish up meme generator in JS file and clean up CSS

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const formElement = document.querySelector("form");
 
+
 formElement.addEventListener("submit", function (event) {
   event.preventDefault();
   const newImage = document.querySelector("#image-url").value;
+  if (newImage === "") {
+    alert("Please submit a URL for the image!");
+    return false;
+  }
   const topText = document.querySelector("#top-caption").value;
   const bottomText = document.querySelector("#bottom-caption").value;
   const newDiv = document.createElement("div");
@@ -18,8 +23,17 @@ formElement.addEventListener("submit", function (event) {
   textDivBottom.innerText = bottomText;
   const parent = document.querySelector("#memes");
   parent.append(newDiv);
+  const button = document.createElement("button");
+  button.addEventListener("click", function (event) {
+      event.target.parentElement.remove();
+  });
+  button.innerText = "Delete";
+  newDiv.append(button);
   newDiv.append(image);
   newDiv.append(textDiv);
   newDiv.append(textDivBottom);
+
+
+  formElement.reset();
 
 });
